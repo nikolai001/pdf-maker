487fa072dd8a591c83f064b4729e1b38e9039919
nikolairaahauge <nira36696@edu.ucl.dk>
2023-09-02 17:50:22 +0200

    Made elements position based on the relative size and position of the canvas element

--- src/components/editor.jsx
+++ src/components/editor.jsx
@@ -15,25 +15,44 @@ export default function Editor() {
     const handleAction = (action) => {
         setAction(action);
       };
+  
+    let parentWidth;
+    let parentHeight;
+    let mouseX;
+    let mouseY;
 
     const clickDocument = (action, event) => {
         const createElement = (clicked, element) => {
-          console.log(clicked.target)
           if (clicked.target.classList.contains('editor__document')) {
             clicked.target.appendChild(element)
+            parentWidth = clicked.target.clientWidth;
+            parentHeight = clicked.target.clientHeight;
+            mouseX = event.clientX - clicked.target.getBoundingClientRect().left;
+            mouseY = event.clientY - clicked.target.getBoundingClientRect().top;
+            console.log('clicked parent',parentWidth,parentHeight,mouseX,mouseY)
           }else {
             clicked.target.parentNode.appendChild(element)
+            parentWidth = clicked.target.parentNode.clientWidth;
+            parentHeight = clicked.target.parentNode.clientHeight;
+            mouseX = event.clientX - clicked.target.parentNode.getBoundingClientRect().left;
+            mouseY = event.clientY - clicked.target.parentNode.getBoundingClientRect().top;
+            console.log('clicked child',parentWidth,parentHeight,mouseX,mouseY)
           }
-          element.style.left = mousePos.X +"px"
-          element.style.top = mousePos.Y +"px"
+          const mouseXPercentage = (mouseX / parentWidth) * 100;
+          const mouseYPercentage = (mouseY / parentHeight) * 100;
+          console.log(mouseXPercentage,mouseYPercentage)
+          element.style.left = mouseXPercentage +"%"
+          element.style.top = mouseYPercentage +"%"
         }
+
         let mousePos = {X:event.clientX, Y:event.clientY}
+
         if ( action === "Shape") {
             let newShape = document.createElement("div");
             newShape.classList.add("document__element");
             createElement(event,newShape)
-            newShape.style.width = '50px'
-            newShape.style.height = '50px'
+            newShape.style.width = '10%'
+            newShape.style.aspectRatio = '1/1'
             newShape.style.backgroundColor = '#000'
         }else if (action === "Text") {
             let newText = document.createElement("textarea");
@@ -46,7 +65,10 @@ export default function Editor() {
       <div className="editor">
         <Toolbar receivedData={handleFont} action={handleAction}/>
         <article className='editor__document' onClick={(e) => clickDocument(action, e)}>
-            <h1 className='document__test' style={{ fontFamily: font }}>Im a test</h1>
+          <div className='document__canvas'>
+            
+          </div>
+            {/* <h1 className='document__test' style={{ fontFamily: font }}>Im a test</h1> */}
         </article>
       </div>
     );
